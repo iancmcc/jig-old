3e25d3a101fa16089b84ec6c594f641d9e4b84bf
Ian McCracken <ian.mccracken@gmail.com>
2015-02-23 20:37:53 -0600

    Scan output, progress

--- git/git.go
+++ git/git.go
@@ -1,10 +1,15 @@
 package git
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
+	"io"
 	"os/exec"
 	"regexp"
 	"strconv"
+	"strings"
+	"sync"
 )
 
 type Progress struct {
@@ -38,16 +43,98 @@ func ParseProgressLine(line string) (*Progress, error) {
 	return p, nil
 }
 
-func (g *Git) Run(args []string) error {
-	cmd := exec.Command("git", args)
+func scanOutput(output io.Reader) <-chan string {
+	outchan := make(chan string)
+	scanner := bufio.NewScanner(output)
+	go func() {
+		defer close(outchan)
+		for scanner.Scan() {
+			outchan <- strings.TrimSpace(scanner.Text())
+		}
+	}()
+	return outchan
 }
 
-func (g *Git) RunWithProgress(args []string) (chan<- Progress, error) {
-	cmd := exec.Command("git", args)
-	cmd.Stderr = nil
-
+func scanProgress(output io.Reader) <-chan Progress {
+	outchan := make(chan Progress)
+	scanner := bufio.NewScanner(output)
+	split := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+		if atEOF && len(data) == 0 {
+			return 0, nil, nil
+		}
+		if i := bytes.IndexByte(data, ''); i >= 0 {
+			// We have a full newline-terminated line.
+			return i + 1, bytes.TrimSpace(data[0:i]), nil
+		}
+		// If we're at EOF, we have a final, non-terminated line. Return it.
+		if atEOF {
+			return len(data), bytes.TrimSpace(data), nil
+		}
+		// Request more data.
+		return 0, nil, nil
+	}
+	scanner.Split(split)
+	go func() {
+		defer close(outchan)
+		for scanner.Scan() {
+			prog, err := ParseProgressLine(scanner.Text())
+			if err != nil {
+				continue
+			}
+			outchan <- *prog
+		}
+	}()
+	return outchan
 }
 
-func (g *Git) Clone(uri string) chan<- Progress {
-	return nil
+func (g *Git) Run(args ...string) (output <-chan string, err error) {
+	var stderr, stdout io.Reader
+	cmd := exec.Command("git", args...)
+	if stdout, err = cmd.StdoutPipe(); err != nil {
+		return nil, err
+	}
+	if stderr, err = cmd.StderrPipe(); err != nil {
+		return nil, err
+	}
+	outchan := scanOutput(stdout)
+	errchan := scanOutput(stderr)
+	combined := make(chan string)
+	var wait sync.WaitGroup
+	wait.Add(2)
+	go func() {
+		defer wait.Done()
+		for line := range outchan {
+			combined <- line
+		}
+	}()
+	go func() {
+		defer wait.Done()
+		for line := range errchan {
+			combined <- line
+		}
+	}()
+	go func() {
+		wait.Wait()
+		close(combined)
+	}()
+	go cmd.Run()
+	return combined, nil
+}
+
+func (g *Git) RunWithProgress(args ...string) (output <-chan string, progress <-chan Progress, err error) {
+	var stdout, stderr io.Reader
+	cmd := exec.Command("git", args...)
+	if stdout, err = cmd.StdoutPipe(); err != nil {
+		return nil, nil, err
+	}
+	if stderr, err = cmd.StderrPipe(); err != nil {
+		return nil, nil, err
+	}
+	output = scanOutput(stdout)
+	progress = scanProgress(stderr)
+	return output, progress, nil
+}
+
+func (g *Git) Clone(uri string) (<-chan string, <-chan Progress, error) {
+	return g.RunWithProgress("clone", uri)
 }
